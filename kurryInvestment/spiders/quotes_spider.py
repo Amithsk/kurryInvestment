b24628a3d571a5ff7bf44ebf661e0de4b0ba4096
Amith <Amith@apples-MacBook-Air.local>
2018-06-29 08:04:51 +0530

    Issue identified:  While dumping the data,the scrapy is creating two list Solution:  removed the ',' after the captured data  Updated the spider to dump the scrapped data in one list onlu
    Initial:
    item['noOfChitty'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[1]//text()').re(r'[0-9,-/]+|[0-9]+'),
    item['monthInfo'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[2]//text()').re(r'[0-9,-/]+|[NKL 0-9]+')
    
    Updated:
    item['noOfChitty'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[1]//text()').re(r'[0-9,-/]+|[0-9]+')
    item['monthInfo'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[2]//text()').re(r'[0-9,-/]+|[NKL 0-9]+')
    
    Need to ask the question in Stack overflow

--- kurryInvestment/spiders/quotes_spider.py
+++ kurryInvestment/spiders/quotes_spider.py
@@ -11,11 +11,11 @@ class QuotesSpider(scrapy.Spider):
         for check in response.xpath('//table[@class="MsoTableGrid"]'):
             item = dict()
             item['noOfChitty'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[1]//text()').re(r'[0-9,-/]+|[0-9]+')
-            item['monthInfo'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[2]//text()').re(r'[0-9,-/]+|[NKL 0-9]+'),
-            item['chittyNo'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[3]//text()').re(r'[0-9/]+|[NKL 0-9]+'),
-            item['regNo'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[4]//text()').re(r'[0-9,-/]+'),
-            item['sala'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[5]//text()').re(r'[0-9,-/]+'),
- 	    item['inst']=check.xpath('//table[@class="MsoTableGrid"]/tr/td[6]//text()').re(r'[0-9/]+|[A-Za-z]+'),	
-            item['chittyStatus']=check.xpath('//table[@class="MsoTableGrid"]/tr/td[7]//text()').re(r'[0-9,]+|[A-Za-z]+'),
+            item['monthInfo'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[2]//text()').re(r'[0-9,-/]+|[NKL 0-9]+')
+            item['chittyNo'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[3]//text()').re(r'[0-9/]+|[NKL 0-9]+')
+            item['regNo'] =check.xpath('//table[@class="MsoTableGrid"]/tr/td[4]//text()').re(r'[0-9,-/]+')
+            item['sala'] = check.xpath('//table[@class="MsoTableGrid"]/tr/td[5]//text()').re(r'[0-9,-/]+')
+ 	    item['inst']=check.xpath('//table[@class="MsoTableGrid"]/tr/td[6]//text()').re(r'[0-9/]+|[A-Za-z]+')	
+            item['chittyStatus']=check.xpath('//table[@class="MsoTableGrid"]/tr/td[7]//text()').re(r'[0-9,]+|[A-Za-z]+')
             item['chittyAmount']=check.xpath('//table[@class="MsoTableGrid"]/tr/td[8]//text()').re(r'[0-9,]+')	
             yield item
